2d8d72c9be15d3b7505a923ed84100f3553d8e45
PitrsScripts <150464632+PitrsScripts@users.noreply.github.com>
2024-11-23 19:58:05 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+version '1.0.0'
+
+
+server_scripts {
+    "@async/async.lua",
+    "@mysql-async/lib/MySQL.lua",
+    'main.lua',
+    'config.lua',
+}
