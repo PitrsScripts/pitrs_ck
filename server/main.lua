c0c57ab6a246c20d38ec588c92aa36110e344667
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2024-12-22 18:46:30 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,88 +1,49 @@
 
 ESX = nil
-QBCore = nil
 
-Config.useESX = Config.Framework == 'ESX'
-Config.useQBCore = Config.Framework == 'QBCore'
-
-
-if Config.useESX then
-    ESX = exports["es_extended"]:getSharedObject()
-    if not ESX then
-        print("DEBUG: ESX is not initialized. Please check if es_extended is running.")
-    else
-        print("DEBUG: ESX has been successfully initialized.")
-    end
-elseif Config.useQBCore then
-    QBCore = exports['qb-core']:GetCoreObject()
-    if not QBCore then
-        print("DEBUG: QBCore is not initialized. Please check if qb-core is running.")
-    else
-        print("DEBUG: QBCore has been successfully initialized.")
-    end
+ESX = exports["es_extended"]:getSharedObject()
+if not ESX then
+    print("DEBUG: ESX is not initialized. Please check if es_extended is running.")
 else
-    print("DEBUG: No framework is set in the configuration. Please check Config.Framework.")
+    print("DEBUG: ESX has been successfully initialized.")
 end
 
-
 RegisterCommand('ck', function(source, args, rawCommand)
-    local xPlayer = nil
-
-    if Config.useESX then
-        xPlayer = ESX.GetPlayerFromId(source)
-    elseif Config.useQBCore then
-        xPlayer = QBCore.Functions.GetPlayer(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    
+    if not xPlayer then
+        TriggerClientEvent('esx:showNotification', source, 'Hráč nebyl nalezen.')
+        return
     end
 
     local hasPermission = false
     for _, group in ipairs(Config.allowedGroups) do
-        if (Config.useESX and xPlayer.getGroup() == group) or (Config.useQBCore and xPlayer.PlayerData.job.name == group) then
+        if xPlayer.getGroup() == group then
             hasPermission = true
             break
         end
     end
 
     if not hasPermission then
-        if Config.useESX then
-            TriggerClientEvent('esx:showNotification', source, 'Nemáš oprávnění k provedení tohoto příkazu.')
-        elseif Config.useQBCore then
-            TriggerClientEvent('QBCore:Notify', source, 'Nemáš oprávnění k provedení tohoto příkazu.', 'error')
-        end
+        TriggerClientEvent('esx:showNotification', source, 'Nemáš oprávnění k provedení tohoto příkazu.')
         return
     end
 
     if not args[1] then
-        if Config.useESX then
-            TriggerClientEvent('esx:showNotification', source, 'Nezadal jsi platné ID.')
-        elseif Config.useQBCore then
-            TriggerClientEvent('QBCore:Notify', source, 'Nezadal jsi platné ID.', 'error')
-        end
+        TriggerClientEvent('esx:showNotification', source, 'Nezadal jsi platné ID.')
         return
     end
 
     local targetId = tonumber(args[1])
     if not targetId then
-        if Config.useESX then
-            TriggerClientEvent('esx:showNotification', source, 'Nezadal jsi platné číslo ID.')
-        elseif Config.useQBCore then
-            TriggerClientEvent('QBCore:Notify', source, 'Nezadal jsi platné číslo ID.', 'error')
-        end
+        TriggerClientEvent('esx:showNotification', source, 'Nezadal jsi platné číslo ID.')
         return
     end
 
-    local targetPlayer = nil
-    if Config.useESX then
-        targetPlayer = ESX.GetPlayerFromId(targetId)
-    elseif Config.useQBCore then
-        targetPlayer = QBCore.Functions.GetPlayer(targetId)
-    end
+    local targetPlayer = ESX.GetPlayerFromId(targetId)
 
     if not targetPlayer then
-        if Config.useESX then
-            TriggerClientEvent('esx:showNotification', source, 'Hráč s tímto ID nebyl nalezen.')
-        elseif Config.useQBCore then
-            TriggerClientEvent('QBCore:Notify', source, 'Hráč s tímto ID nebyl nalezen.', 'error')
-        end
+        TriggerClientEvent('esx:showNotification', source, 'Hráč s tímto ID nebyl nalezen.')
         return
     end
 
@@ -98,18 +59,10 @@ RegisterCommand('ck', function(source, args, rawCommand)
                 ['@identifier'] = identifier
             }, function(rowsChanged)
                 if rowsChanged > 0 then
-                    if Config.useESX then
-                        TriggerClientEvent('esx:showNotification', source, 'Na postavu s ID ' .. targetId .. ' bylo uděleno CK.')
-                    elseif Config.useQBCore then
-                        TriggerClientEvent('QBCore:Notify', source, 'Na postavu s ID ' .. targetId .. ' bylo uděleno CK.', 'success')
-                    end
+                    TriggerClientEvent('esx:showNotification', source, 'Na postavu s ID ' .. targetId .. ' bylo uděleno CK.')
                     DropPlayer(targetId, 'Dostal jsi CK na postavu.')
                 else
-                    if Config.useESX then
-                        TriggerClientEvent('esx:showNotification', source, 'Nastala chyba při provád ění CK.')
-                    elseif Config.useQBCore then
-                        TriggerClientEvent('QBCore:Notify', source, 'Nastala chyba při provádění CK.', 'error')
-                    end
+                    TriggerClientEvent('esx:showNotification', source, 'Nastala chyba při provádění CK.')
                 end
             end)
         end)
